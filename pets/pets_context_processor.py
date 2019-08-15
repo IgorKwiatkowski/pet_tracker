9ec24020e6881697bf1bebbad26be33e93d9b9e8
Igor Kwiatkowski <waffel@gmail.com>
2019-08-15 14:18:11 +0200

    added tomorrow link

--- pets/pets_context_processor.py
+++ pets/pets_context_processor.py
@@ -1,9 +1,12 @@
+from datetime import date, timedelta
 from pets.models import Pet, Product
 
 
 def pets_context_processor(request):
     all_pets = Pet.objects.all()
     all_products = Product.objects.all()
+    tomorrow = date.today() + timedelta(days=1)
     ctx = {'all_pets': all_pets,
-           'all_products': all_products}
+           'all_products': all_products,
+           'tomorrow': tomorrow}
     return ctx
